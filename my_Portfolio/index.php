b342e78adbf8727752a9c7016c4b7dd2e317e149
Sandeep Maji <68271347+build-with-sandeep@users.noreply.github.com>
2021-04-03 21:57:25 +0530

    update index.php

--- my_Portfolio/index.php
+++ my_Portfolio/index.php
@@ -18,7 +18,7 @@
 
 </head>
 
-<body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('https://source.unsplash.com/1L71sPT5XKc');">
+<body class="font-sans antialiased text-gray-900 leading-normal tracking-wider bg-cover" style="background-image:url('Photos/background1.jpg');">
 
 
 
@@ -30,26 +30,26 @@
 
 		<div class="p-4 md:p-12 text-center lg:text-left">
 			<!-- Image for mobile view-->
-			<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('https://source.unsplash.com/MP0IUfwrn0A')"></div>
+			<div class="block lg:hidden rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center" style="background-image: url('Photos/profile2.jpg')"></div>
 			
 			<h1 class="text-3xl font-bold pt-8 lg:pt-0">Sandeep Kumar Maji</h1>
 			<div class="mx-auto lg:mx-0 w-4/5 pt-3 border-b-2 border-green-500 opacity-25"></div>
-			<p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start"><svg class="h-4 fill-current text-green-700 pr-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z"/></svg>pursuing Masters @ NIT Raipur</p>
+			<p class="pt-4 text-base font-bold flex items-center justify-center lg:justify-start">pursuing Masters @ NIT Raipur</p>
 			
-			<p class="pt-8 text-sm">Hii ,I am a CS Postgrad student ,who likes to solve Problems using some tricky algorithms.</p>
+			<p class="pt-8 text-sm">Hii ,I am a backend developer and programmer  ,who likes to build websites and solve Problems using some tricky and beautiful algorithms ;)</p>
 
 			<div class="pt-12 pb-8">
-				<button class="bg-green-700 hover:bg-green-900 text-white font-bold py-2 px-4 rounded-full">
-				  Get In Touch
+				<button class="bg-blue-700 hover:bg-blue-900 text-white font-bold py-2 px-4 rounded-full">
+				  Know More
 				</button> 
 			</div>
 
 			<div class="mt-6 pb-16 lg:pb-0 w-4/5 lg:w-full mx-auto flex flex-wrap items-center justify-between">
-				<a  class ="link" href="https://www.linkedin.com/in/sandeep-maji-a1578b1b4/"><img src="https://img.icons8.com/cute-clipart/64/000000/linkedin.png"></a>
+				<a  class ="link" href="https://www.linkedin.com/in/sandeep-maji-a1578b1b4/"><img src="https://img.icons8.com/metro/42/000000/linkedin.png"></a>
 
-				<a class="link" href="#" ><img src="https://img.icons8.com/material-sharp/48/000000/github.png"/></a>
+				<a class="link" href="#" ><img src="https://img.icons8.com/metro/48/000000/github.png"/></a>
 
-				<a class="link" href="#" ><img src="https://img.icons8.com/cute-clipart/64/000000/twitter.png"/></a>
+				<a class="link" href="#" ><img src="https://img.icons8.com/metro/48/000000/twitter.png"/></a>
 				
 				<a href=""></a>
 				<a href=""></a>
@@ -65,7 +65,7 @@
 	<!--Img Col-->
 	<div class="w-full lg:w-2/5">
 		<!-- Big profile image for side bar (desktop) -->
-		<img src="Photos/profile1.jpg" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
+		<img src="Photos/profile2.jpg" class="rounded-none lg:rounded-lg shadow-2xl hidden lg:block">
 		
 	</div>
 	
@@ -79,7 +79,7 @@
 
 	<script src="https://unpkg.com/popper.js@1/dist/umd/popper.min.js"></script>
 	<script src="https://unpkg.com/tippy.js@4"></script>
-	<script>
+	/<script>
 		//Init tooltips
 		tippy('.link',{
 		  placement: 'bottom'
